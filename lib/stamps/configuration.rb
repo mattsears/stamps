bc05ffe91c2e6c961ee92af6f434bf326a2bd4bc
Charles Palleschi <charles.palleschi@sparkwiresolutions.com>
2012-05-22 17:44:17 -0400

    Temp - Testing namespace

--- lib/stamps/configuration.rb
+++ lib/stamps/configuration.rb
@@ -20,7 +20,8 @@ module Stamps
     DEFAULT_ENDPOINT = 'https://swsim.testing.stamps.com/swsim/SwsimV22.asmx'.freeze # Test endpoint
     
     # The default namespace used on Stamps.com wsdl
-    DEFAULT_NAMESPACE = 'http://stamps.com/xml/namespace/2010/11/swsim/swsimv12'
+    #DEFAULT_NAMESPACE = 'http://stamps.com/xml/namespace/2010/11/swsim/swsimv12'
+    DEFAULT_NAMESPACE = 'http://testing.stamps.com/xml/namespace/2010/11/swsim/swsimv12' # Test namespace
 
     # @note JSON is preferred over XML because it is more concise and faster to parse.
     DEFAULT_FORMAT = :hash
