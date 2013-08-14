f7a85260a08d28ea21b07c7dc7701cae5e7ea8b4
Corey Trombley <corey.trombley33@gmail.com>
2013-08-14 15:19:39 -0400

    Updated default endpoint and namespace to latest version, v29

--- test/request_test.rb
+++ test/request_test.rb
@@ -5,10 +5,9 @@ class RequestTest < Test::Unit::TestCase
 	context "get_authenticator_token" do
 		context "with invalid authentication token" do
       setup do
-        @response = stub_response("failed_auth")
         stub_request(:post, "https://stamps.example.com/swsim/swsimv12.asmx").
-        with(:body => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://stamps.com/xml/namespace/2010/11/swsim/swsimv12\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><tns:AuthenticateUser><tns:Credentials><tns:IntegrationID>XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX</tns:IntegrationID><tns:Username>USERNAME</tns:Username><tns:Password>PASSXXXX</tns:Password></tns:Credentials></tns:AuthenticateUser></soap:Body></soap:Envelope>",
-        :headers => {'Accept'=>'*/*', 'Content-Type'=>'text/xml;charset=UTF-8', 'Soapaction'=>'http://stamps.com/xml/namespace/2010/11/swsim/swsimv12/AuthenticateUser', 'User-Agent'=>'Ruby'}).
+        with(:body => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://stamps.com/xml/namespace/2010/11/swsim/swsimv29\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><tns:AuthenticateUser><tns:Credentials><tns:IntegrationID>XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX</tns:IntegrationID><tns:Username>USERNAME</tns:Username><tns:Password>PASSXXXX</tns:Password></tns:Credentials></tns:AuthenticateUser></soap:Body></soap:Envelope>", 
+        :headers => {'Accept'=>'*/*', 'Content-Type'=>'text/xml;charset=UTF-8', 'Soapaction'=>'http://stamps.com/xml/namespace/2010/11/swsim/swsimv29/AuthenticateUser', 'User-Agent'=>'Ruby'}).
         to_return(:status => 200, :body => "", :headers => {})
       end
 
